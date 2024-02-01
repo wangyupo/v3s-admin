4636ad71908a1e1da8fda29aedb487cb10ec9a43
融合应用事业部-王玉珀 <1106774262@qq.com>
2024-02-01 14:56:27 +0800

    update

--- assets/home-dpa3rT2w.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,o as t,d as o,e}from"./index-HY4yjMde.js";const s={},n=e("p",null,"这是首页，请尝试使用 Ctrl + Space（空格键） 唤醒菜单搜索，搜索并进入你想要的页面🧐。",-1),a=e("p",{class:"flex items-center"},"（注意：如果你的电脑是 mac/mbp，请使用 command + k 来唤醒菜单搜索。）",-1),_=[n,a];function r(l,d){return t(),o("div",null,_)}const i=c(s,[["render",r]]);export{i as default};
