8bf65c71d8f9ef59af610bb3cc8a92712c703a09
融合应用事业部-王玉珀 <1106774262@qq.com>
2024-03-13 11:28:48 +0800

    update

--- assets/index-vkfQkehV.js
+++
@@ -1,1 +0,0 @@
-import{a as n,c as s,o as c,d as r,j as e,f as l,g as p}from"./index-TaY89MIF.js";const m={__name:"index",setup(u){const t=n(),o=()=>{t.push({path:"/example/page/child",query:{id:"1",type:"2"}})};return(_,d)=>{const a=s("el-button");return c(),r("div",null,[e(" 主页面 "),l(a,{onClick:o},{default:p(()=>[e("点我跳转子页面")]),_:1})])}}};export{m as default};
