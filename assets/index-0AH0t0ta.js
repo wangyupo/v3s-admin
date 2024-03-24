3a75c4c05746a409a852614f0b561f93c731a3db
融合应用事业部-王玉珀 <1106774262@qq.com>
2024-03-24 16:25:02 +0800

    update

---
+++ assets/index-0AH0t0ta.js
@@ -0,0 +1,1 @@
+import{a as n,c as s,o as c,d as r,k as e,f as l,g as p}from"./index-zPWwkCPW.js";const m={__name:"index",setup(u){const t=n(),o=()=>{t.push({path:"/example/page/child",query:{id:"1",type:"2"}})};return(_,d)=>{const a=s("el-button");return c(),r("div",null,[e(" 主页面 "),l(a,{onClick:o},{default:p(()=>[e("点我跳转子页面")]),_:1})])}}};export{m as default};
